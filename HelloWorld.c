770a8f57ad8079cd4183c2a3d768a4dd967d25cc
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2023-06-27 08:41:03 +0530

    c program

--- HelloWorld.c
+++ HelloWorld.c
@@ -4,5 +4,7 @@ void main()
     signed short int a=-32769;
     printf("%d\n",a);
     unsigned short int b=65538;
-    printf("%d",b);
+    printf("%d\n",b);
+    short int c=10;
+    printf("%d is %d",c,c);
 }
